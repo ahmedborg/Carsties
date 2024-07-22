e42a80d1368cfebd84b01b2941fb36cd1100009a
Ahmed Borg <ahmedborg@gmail.com>
2024-07-22 15:45:51 -0700

    End of section 4

--- src/AuctionService/Data/AuctionDbContext.cs
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -1,5 +1,5 @@
 using AuctionService.Entities;
-//using MassTransit;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 namespace AuctionService.Data;
@@ -12,12 +12,12 @@ public class AuctionDbContext : DbContext
 
     public DbSet<Auction> Auctions { get; set; }
 
-    // protected override void OnModelCreating(ModelBuilder modelBuilder)
-    // {
-    //     base.OnModelCreating(modelBuilder);
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
 
-    //     modelBuilder.AddInboxStateEntity();
-    //     modelBuilder.AddOutboxMessageEntity();
-    //     modelBuilder.AddOutboxStateEntity();
-    // }
+        modelBuilder.AddInboxStateEntity();
+        modelBuilder.AddOutboxMessageEntity();
+        modelBuilder.AddOutboxStateEntity();
+    }
 }
